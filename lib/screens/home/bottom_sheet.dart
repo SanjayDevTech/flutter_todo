9de1e97e0d0f9a341ef5558e2956d654a04b4087
SanjayDevTech <sureshsanjay805@gmail.com>
2022-11-27 10:47:48 +0530

    added title in bottom modal sheet

--- lib/screens/home/bottom_sheet.dart
+++ lib/screens/home/bottom_sheet.dart
@@ -1,44 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_todo/data/models.dart';
 
 class TodoAddBottomSheet extends StatelessWidget {
   TodoAddBottomSheet({super.key});
 
-  // final void Function(String, String) onAdd;
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextFormField(
-              controller: _titleController,
-              decoration: const InputDecoration(
-                labelText: "Title",
-              ),
+    return Container(
+      height: 300,
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          const Text("Add todo", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+          TextFormField(
+            controller: _titleController,
+            decoration: const InputDecoration(
+              labelText: "Title",
             ),
-            TextFormField(
-              controller: _descriptionController,
-              decoration: const InputDecoration(
-                labelText: "Description",
-              ),
+          ),
+          TextFormField(
+            controller: _descriptionController,
+            decoration: const InputDecoration(
+              labelText: "Description",
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context, {"title": _titleController.text, "description": _descriptionController.text});
-                  
-                },
-                child: const Text("Add"),
-              ),
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context, {"title": _titleController.text, "description": _descriptionController.text});
+              },
+              child: const Text("Add"),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/home/data.dart
+++ lib/screens/home/data.dart
@@ -23,30 +23,36 @@ class HomeData extends StatelessWidget {
     return Column(
       children: [
         Flexible(
-          child: todos.isEmpty ? const Center(child: Text("No Todo to show here!"),) : ListView.builder(
-            key: const ValueKey("ListViewKey"),
-            itemCount: todos.length,
-            itemBuilder: (context, index) {
-              var todo = todos[index];
-              return TodoItem(
-                key: ValueKey(todo.id),
-                todo: todo,
-                onRemove: () {
-                  removeTodo(todo.id);
-                },
-                onComplete: () {
-                  completeTodo(todo.id);
-                },
-              );
-            },
-          ),
+          child: todos.isEmpty
+              ? const Center(
+                  child: Text("No Todo to show here!"),
+                )
+              : ListView.builder(
+                  key: const ValueKey("ListViewKey"),
+                  itemCount: todos.length,
+                  itemBuilder: (context, index) {
+                    var todo = todos[index];
+                    return TodoItem(
+                      key: ValueKey(todo.id),
+                      todo: todo,
+                      onRemove: () {
+                        removeTodo(todo.id);
+                      },
+                      onComplete: () {
+                        completeTodo(todo.id);
+                      },
+                    );
+                  },
+                ),
         ),
         Flexible(
           flex: 0,
           child: ElevatedButton(
             onPressed: () async {
               var data = await showModalBottomSheet<Map<String, String>>(
-                  context: context, builder: (_) => TodoAddBottomSheet());
+                context: context,
+                builder: (_) => TodoAddBottomSheet(),
+              );
               if (data != null) {
                 var todo = Todo(
                   id: nextId(),
